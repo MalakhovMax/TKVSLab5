1484672d4f5aa673e176f1ec4a2838d68cc48be0
MalakhovMax <90110212+MalakhovMax@users.noreply.github.com>
2023-06-03 16:48:10 +0300

    Delete metanit directory

--- metanit/lab3tkvs/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
-class AddOwn(forms.Form):
-    name = forms.CharField(label="Name")
-    package = forms.IntegerField(label="Number of package")
-    drug = forms.CharField(label="Name of drug")
-    phone = forms.IntegerField(label="Phone number")
-    address = forms.CharField(label="Address")
--- metanit/lab3tkvs/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Own(models.Model):
-    name = models.CharField(max_length=120)
-    package = models.IntegerField()
-    drug = models.CharField(max_length=120)
-    phone = models.IntegerField()
-    address = models.CharField(max_length=1000)
